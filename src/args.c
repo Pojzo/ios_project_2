054f27a33e9f6e74b4fde564902700e6dddb1c70
Pojzo <pojzinko8@gmail.com>
2022-04-12 16:55:57 +0200

    Minor changes

--- src/args.c
+++ src/args.c
@@ -4,11 +4,11 @@
 #include <stdio.h>
 #include <string.h>
 
-const int MIN_TB = 0;
-const int MAX_TB = 1000;
+const static int MIN_TB = 0;
+const static int MAX_TB = 1000;
 
-const int MIN_TI = 0;
-const int MAX_TI = 1000;
+const static int MIN_TI = 0;
+const static int MAX_TI = 1000;
 
 args_t *args_create(const char *num_oxygen_, const char *num_hydrogen_, const char *TI_, const char *TB_) {
     args_t *args = (args_t *) malloc(sizeof(args_t));
@@ -28,22 +28,22 @@ args_t *args_create(const char *num_oxygen_, const char *num_hydrogen_, const ch
 
 // each argument has to be a number
 bool args_valid(const char *num_oxygen, const char *num_hydrogen, const char *TI, const char *TB) {
-    if (strlen(num_oxygen) > 0 && !is_numeric(num_oxygen)) {
+    if (!is_numeric(num_oxygen)) {
         fprintf(stderr, "NO argument is not valid\n");
         return false;
     }
 
-    if (strlen(num_oxygen) > 0 && !is_numeric(num_hydrogen)) {
+    if (!is_numeric(num_hydrogen)) {
         fprintf(stderr, "NH argument is not valid\n");
         return false;
     }
 
-    if (strlen(TI) > 0 && !is_numeric(TI)) {
+    if (!is_numeric(TI)) {
         fprintf(stderr, "TI argument is not valid\n");
         return false;
     }
 
-    if (strlen(TB) > 0 && !is_numeric(TB)) {
+    if (!is_numeric(TB)) {
         fprintf(stderr, "TB argument is not valid\n");
         return false;
     }
