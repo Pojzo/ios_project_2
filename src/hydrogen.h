e65ed89026f66e8a34db83dcf595c7002329b000
Pojzo <pojzinko8@gmail.com>
2022-04-18 18:34:35 +0200

    Removed oxygen and hydrogen

--- src/hydrogen.h
+++
@@ -1,7 +0,0 @@
-#ifndef HYDROGEN_H
-#define HYDROGEN_H
-#include <semaphore.h>
-
-void hydrogen_process(int pid, int TI, sem_t *sem_start);
-
-#endif
--- src/oxygen.h
+++
@@ -1,7 +0,0 @@
-#ifndef OXYGEN_H
-#define OXYGEN_H
-#include <semaphore.h>
-
-void oxygen_process(int pid, int TI, sem_t *sem_start);
-
-#endif
