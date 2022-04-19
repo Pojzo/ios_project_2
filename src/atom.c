0562f85cdb288d7979a4b9691240eb3b5f671ca8
Pojzo <pojzinko8@gmail.com>
2022-04-19 15:37:28 +0200

    Added atom_create and atom_queue

--- src/atom.c
+++ src/atom.c
@@ -4,16 +4,28 @@
 #include "common.h"
 #include "atom.h"
 
-void atom_process(char atom, int pid, int TI, sem_t *sem_start, int *ptr) {
-    (void) ptr;
+void atom_process(char atom, int pid, int TI, sem_t *sem_start, data_t *data_ptr) {
     srand(getpid());
+    int *line_num_ptr = &(data_ptr->line_num);
 
+    atom_start(atom, pid, sem_start, line_num_ptr);
+    atom_queue(atom, pid, TI, sem_start, line_num_ptr);
+
+}
+
+// start atom process and log a message
+void atom_start(char atom, int pid, sem_t *sem_start, int *line_num_ptr) {
     sem_wait(sem_start);
+    log_line(line_num_ptr);
     log_started(atom, pid);
     sem_post(sem_start);
+}
 
+// add atom to queue and log a message
+void atom_queue(char atom, int pid, int TI, sem_t *sem_start, int *line_num_ptr) {
     sem_wait(sem_start);
     random_sleep_ms(0, TI);
+    log_line(line_num_ptr);
     log_queue(atom, pid);
     sem_post(sem_start);
 }
--- src/atom.h
+++ src/atom.h
@@ -1,7 +1,9 @@
 #ifndef OXYGEN_H
 #define OXYGEN_H
 #include <semaphore.h>
+#include "shared_memory.h"
 
-void atom_process(char atom, int pid, int TI, sem_t *sem_start, int *ptr);
-
+void atom_process(char atom, int pid, int TI, sem_t *sem_start, data_t *data_ptr);
+void atom_start(char atom, int pid, sem_t *sem_start, int *line_num_ptr);
+void atom_queue(char atom, int pid, int IT, sem_t *sem_start, int *line_num_ptr);
 #endif
