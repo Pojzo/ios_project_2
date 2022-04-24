2e039aa60a11b006599db3a3421d4d17a718109c
Pojzo <pojzinko8@gmail.com>
2022-04-24 17:17:14 +0200

    Changed two functions

--- src/logger.c
+++ src/logger.c
@@ -55,14 +55,14 @@ void log_molecule_created(char atom, int atom_idx, data_t *data_ptr) {
     sem_post(&data_ptr->sem_print);
 }
 
-void log_not_enough_one(char first_atom, int atom_idx, char second_atom, data_t *data_ptr) {
+void log_not_enough_two(char atom, int atom_idx, data_t *data_ptr) {
     sem_wait(&(data_ptr->sem_print));
-    printf("%d: %c %d: not enough %c\n", data_ptr->line_num++, first_atom, atom_idx, second_atom);
+    printf("%d: %c %d: not enough O or H\n", data_ptr->line_num++, atom, atom_idx);
     sem_post(&(data_ptr->sem_print));
 }
 
-void log_not_enough_two(char first_atom, int atom_idx, char second_atom, char third_atom, data_t *data_ptr) {
+void log_not_enough_one(char atom, int atom_idx, data_t *data_ptr) {
     sem_wait(&(data_ptr->sem_print));
-    printf("%d: %c %d: not enough %c or %c\n", data_ptr->line_num++, first_atom, atom_idx, second_atom, third_atom);
+    printf("%d: %c %d: not enough H\n", data_ptr->line_num++, atom, atom_idx);
     sem_post(&(data_ptr->sem_print));
 }
--- src/logger.h
+++ src/logger.h
@@ -9,7 +9,7 @@ void log_started_queue(char atom, int atom_idx, data_t *data_ptr, int action);
 void log_queue(char atom, int atom_idx, data_t *data_ptr);
 void log_molecule_started(char atom, int atom_idx, data_t *data_ptr);
 void log_molecule_created(char atom, int atom_idx, data_t *data_ptr);
-void log_not_enough_one(char atom, int atom_idx, char second_atom, data_t *data_ptr);
-void log_not_enough_two(char atom, int atom_idx, char second_atom, char third_atom, data_t *data_ptr);
 
+void log_not_enough_two(char atom, int atom_idx, data_t *data_ptr);
+void log_not_enough_one(char atom, int atom_idx, data_t *data_ptr);
 #endif
