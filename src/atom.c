26f0e1a1abfbd6536e52cf988ca8acb656018afc
Pojzo <pojzinko8@gmail.com>
2022-04-25 11:25:45 +0200

    Sort of works

--- src/atom.c
+++ src/atom.c
@@ -8,11 +8,15 @@ void oxygen_process(char atom, int atom_idx, data_t *data_ptr) {
     log_started(atom, atom_idx, data_ptr); 
     random_sleep_ms(0, data_ptr->args->TI); 
     log_queue(atom, atom_idx, data_ptr);
+    if (data_ptr->args->num_hydrogen - data_ptr->num_queued_h < 2) {
+        log_not_enough_one(atom, atom_idx, data_ptr);
+        return;
+    }
+    data_ptr->num_queued_o += 1;
+
     sem_wait(&data_ptr->sem_mol);
     log_molecule_started(atom, atom_idx, data_ptr);
-    // sem_post(&data_ptr->sem_oxygen);
-    // sem_post(&data_ptr->sem_mol);
-    // return;
+
     data_ptr->num_oxygen += 1;
     if (data_ptr->num_hydrogen >= 2) {
         sem_post(&data_ptr->sem_hydrogen);
@@ -22,7 +26,6 @@ void oxygen_process(char atom, int atom_idx, data_t *data_ptr) {
         sem_post(&data_ptr->sem_oxygen);
         data_ptr->num_oxygen -= 1;
         random_sleep_ms(0, data_ptr->args->TB);
-        data_ptr->mol_num++;
     }
     else {
         sem_post(&data_ptr->sem_mol);
@@ -31,18 +34,22 @@ void oxygen_process(char atom, int atom_idx, data_t *data_ptr) {
 
     bond(atom, atom_idx, data_ptr);
 
+    data_ptr->mol_num++;
     sem_post(&data_ptr->sem_oxygen);
-    log_molecule_created(atom, atom_idx, data_ptr);
+    //log_molecule_created(atom, atom_idx, data_ptr);
 
     sem_post(&data_ptr->sem_mol);
     sem_wait(&data_ptr->barrier);
-    data_ptr->mol_num++;
 }
 
 void hydrogen_process(char atom, int atom_idx, data_t *data_ptr) {
     log_started(atom, atom_idx, data_ptr); 
     random_sleep_ms(0, data_ptr->args->TI); 
     log_queue(atom, atom_idx, data_ptr);
+    if (data_ptr->args->num_hydrogen - data_ptr->num_queued_h < 2) {
+        log_not_enough_two(atom, atom_idx, data_ptr);
+        return;
+    }
     data_ptr->num_queued_h++;
     sem_wait(&data_ptr->sem_mol);
     log_molecule_started(atom, atom_idx, data_ptr);
@@ -58,7 +65,6 @@ void hydrogen_process(char atom, int atom_idx, data_t *data_ptr) {
         sem_post(&data_ptr->sem_oxygen);
         data_ptr->num_oxygen -= 1;
         random_sleep_ms(0, data_ptr->args->TB);
-        data_ptr->mol_num++;
     }
     else {
         sem_post(&data_ptr->sem_mol);
@@ -70,7 +76,6 @@ void hydrogen_process(char atom, int atom_idx, data_t *data_ptr) {
 }
 
 void bond(char atom, int atom_idx, data_t *data_ptr) {
-    sem_wait(&data_ptr->barrier);
     log_molecule_created(atom, atom_idx, data_ptr);
     sem_post(&data_ptr->barrier);
 }
